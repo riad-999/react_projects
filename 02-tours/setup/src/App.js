a97a8c876744b4a3cfecc670031fe8f785641598
Riad <felihriad5@gmail.com>
2021-08-31 00:31:41 +0200

    fiishined tours project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -3,9 +3,48 @@ import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading,set_loading] = useState(true);
+  const [tours,set_tours] = useState([]);
+  function remove_tour(id){
+    const new_tours = tours.filter((tour) => tour.id !== id);
+    set_tours(new_tours);
+  }
+  async function get_tours(){
+    set_loading(true);
+    const response = await fetch(url);
+    if(response.status >= 200 && response.status <= 299){
+      const tours = await response.json();
+      console.log(tours);
+      set_loading(false);
+      set_tours(tours);
+    }
+    else{
+      throw new Error(response.statusText);
+    }
+  }
+  useEffect(() => { 
+    get_tours().catch((err)=>console.log(err));
+  },[]);
+  if(loading){
+    return <main>
+        <Loading/>
+      </main>;
+  }
+  if(tours.length === 0){
+    return <main>
+      <div className="title">
+        <h2>no tours left</h2>
+        <button type="button" className="btn" onClick={get_tours}>
+          Refresh
+        </button>
+      </div>
+    </main>
+  }
+  return <main>
+          <Tours tours = {tours} remove_tour = {remove_tour}/>
+        </main>
 }
 
 export default App
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours,remove_tour}) => {
+  return <section>
+    <div className="title">
+      <h2>our tours</h2>
+      <div className="underline"></div>
+    </div>
+    <div>
+      {tours.map(tour => {
+        return <Tour key={tour.id} {...tour} remove_tour = {remove_tour}/>
+      })}
+    </div>
+  </section>;
 };
 
 export default Tours;
