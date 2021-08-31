0285ff8c368d0d3decca8de32b89f4b6e7711f02
Riad <felihriad5@gmail.com>
2021-09-01 00:16:49 +0200

    menu project completed

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -4,7 +4,29 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu_items,set_menu_items] = useState(items);
+  function filter(category){
+    if(category === "all"){
+      set_menu_items(items);
+      return;
+    }
+    const new_items = items.filter(item => {
+      return item.category === category;
+    });
+    set_menu_items(new_items);
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories items={items} filter={filter}/>
+        <Menu items={menu_items}/>
+      </section>
+    </main>
+  );
 }
 
 export default App;
